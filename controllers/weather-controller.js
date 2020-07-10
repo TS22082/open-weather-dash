78d5bcba8ab0fdd0ff75f66453fa33b7bad526e3
TS22082 <ts22082@gmail.com>
2020-07-10 10:59:38 -0700

    redirect user if not logged in

--- controllers/weather-controller.js
+++ controllers/weather-controller.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 
 module.exports = {
   getWeather: async (req, res) => {
+    const base_url = "https://api.openweathermap.org/data/2.5/weather";
     if (!req.user) {
-      res.send("Not authorized");
+      res.redirect("/");
     } else {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?zip=${req.user.zip},us&appid=${process.env.WEATHER_KEY}`
+          `${base_url}?zip=${req.user.zip},us&appid=${process.env.WEATHER_KEY}`
         );
         res.send(response.data);
       } catch (err) {
